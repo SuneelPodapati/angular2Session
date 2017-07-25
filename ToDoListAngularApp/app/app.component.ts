41286cf93a6a09baa68d1a1631498da2324cedde
A07136_h4.03.26 <A07136_h4.03.26@accenture.com>
2017-07-25 12:42:07 +0530

    First commit

---
+++ ToDoListAngularApp/app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+@Component ({
+    selector : "app-todo",
+    template : `<div class="bg-warning">ToDo List will come here...</div>`
+})
+
+export class AppComponent{
+    
+}
