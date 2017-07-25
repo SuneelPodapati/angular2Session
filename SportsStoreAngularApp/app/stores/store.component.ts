c59142ce0f77657bf6c37921a1c0b4a383773fa8
A07136_h4.03.26 <A07136_h4.03.26@accenture.com>
2017-07-25 17:27:02 +0530

    Day 2 eod

---
+++ SportsStoreAngularApp/app/stores/store.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+import { Product } from "../models/product.model";
+import { ProductRepository } from "../models/product.repository";
+
+@Component(
+    {
+        selector: "stores",
+        moduleId: module.id,
+        templateUrl: "store.component.html"
+    }
+)
+
+export class StoreComponent{
+    
+}
